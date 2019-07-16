269db9891e5b6db527e527ed002b78a3a85b5b36
Ping Chen <remotenonsense@gmail.com>
2019-07-16 19:14:58 +0900

    Add some documentation

--- src/CSS/Property.purs
+++ src/CSS/Property.purs
@@ -3,6 +3,13 @@ module CSS.Property
   , getValues
   ) where
 
+-- | Get a list of CSS properties.
 foreign import properties :: Array String
 
+-- | Get value keywords of a property.
+-- |
+-- | Note there is no validation in this function. Empty array is returned for
+-- | invalid property. You can use
+-- | [purescript-css-validate](https://pursuit.purescript.org/packages/purescript-css-validate)
+-- | for validation.
 foreign import getValues :: String -> Array String
