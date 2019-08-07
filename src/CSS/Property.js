4ae7fb4cf22e8eac9d5674dda03475658e69b350
PanAeon <vitalii.voloshyn@gmail.com>
2019-08-07 23:01:30 +0300

    Handle color keywords. Upgrade css-tree to 1.0.0-alpha.34

--- src/CSS/Property.js
+++ src/CSS/Property.js
@@ -1,17 +1,146 @@
 var syntax = require("css-tree").lexer;
+var grammar = require("css-tree").grammar;
 
 var properties = Object.keys(syntax.properties);
 
 exports.properties = properties;
 
-exports.getValues = function(property) {
+
+// build map { t0: [t1,...tN] } where t1..tN are directly referenced by t0
+function getAllTypeReferences() {
+
+  function typeReferences(term) {
+    const result = []
+    grammar.walk(term, {
+      enter: node => {
+        if (node.type === 'Type') {
+          result.push(node.name)
+        }
+      }
+    });
+    return result;
+  }
+
+  const result = {}
+  for (t in syntax.types) {
+    const type = syntax.types[t];
+    if (type && type.syntax !== null) {
+      result[type.name] = typeReferences(type.syntax)
+    }
+  }
+  return result;
+}
+
+var _colorTypes = undefined
+
+function getColorTypes() {
+  if (_colorTypes !== undefined) {
+    return _colorTypes;
+  } else {
+    const typeReferences = getAllTypeReferences();
+    const resolved = { color: true }
+
+    function isColor(typeName, visited) {
+      if (resolved[typeName] !== undefined) {
+        return resolved[typeName];
+      } else {
+        if (visited[typeName]) {
+          return false;
+        } else {
+          visited[typeName] = true;
+          if (typeReferences[typeName] !== undefined) {
+            const res = exists(typeReferences[typeName], y => isColor(y, visited))
+            resolved[typeName] = res
+            return res
+          } else {
+            resolved[typeName] = false;
+          }
+        }
+      }
+    }
+
+    _colorTypes = [...['color'], ...Object.keys(typeReferences).filter(x => isColor(x, {}) )]
+    return _colorTypes;
+  }
+}
+
+exports.getColorTypes = getColorTypes;
+
+var _colorProperties = undefined;
+
+function getColorProperties() {
+
+  if (_colorProperties !== undefined) {
+    return _colorProperties;
+  }
+
+  const colorTypes = getColorTypes();
+
+  const resolvedProperties = {}
+  const traversed = {}
+
+  function propertyIsColor(term) {
+    switch (term.type) {
+      case 'Property':
+          if (resolvedProperties[term.name] !== undefined) {
+            return resolvedProperties[term.name];
+          } else {
+            if (traversed[term.name] !== undefined) {
+              return false;
+            } else {
+              traversed[term.name] = true;
+              const isColor =  propertyIsColor(syntax.properties[term.name].syntax)
+              resolvedProperties[term.name] = isColor;
+              return isColor;
+            }
+          }
+      case 'Type':
+        return colorTypes.includes(term.name)
+      case 'Multiplier':
+        return propertyIsColor(term.term)
+      case 'Group':
+        return exists(term.terms, propertyIsColor)
+    }
+  }
+
+  _colorProperties = Object.keys(syntax.properties).filter(p => propertyIsColor(syntax.properties[p].syntax));
+  return _colorProperties;
+}
+
+exports.getColorProperties = getColorProperties;
+
+function getColorValues() {
+  const colors = syntax.types['named-color'].syntax.terms
+    .map(x => x.name)
+    .filter(x => x !== '-non-standard-color');
+  colors.push('currentColor')
+  return colors.sort();
+}
+
+exports.getColorValues = getColorValues;
+
+exports.getValues = function (property) {
   if (!properties.includes(property)) return [];
 
+  if(getColorProperties().includes(property)) return getColorValues();
+
   return syntax.properties[property].syntax.terms
-    .filter(function(term) {
+    .filter(function (term) {
       return term.type == "Keyword";
     })
-    .map(function(term) {
+    .map(function (term) {
       return term.name;
     });
 };
+
+// ------------------------- helper functions -----------------
+
+
+function exists(xs, predicate) {
+  for (var i = 0; i < xs.length; i++) {
+    if (predicate(xs[i])) {
+      return true;
+    }
+  }
+  return false;
+}
--- test/Main.purs
+++ test/Main.purs
@@ -3,8 +3,9 @@ module Test.Main where
 import Prelude
 
 import CSS.Property (getValues)
+import Data.Foldable (sequence_)
 import Effect (Effect)
-import Jest (expectToEqual, test)
+import Jest (expectToEqual, test, describe)
 
 positionValues :: Array String
 positionValues =
@@ -16,8 +17,110 @@ positionValues =
   , "-webkit-sticky"
   ]
 
+colorValues :: Array String
+colorValues = ["aliceblue", "antiquewhite", "aqua", "aquamarine"
+  , "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet"
+  , "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral"
+  , "cornflowerblue", "cornsilk", "crimson", "currentColor", "cyan", "darkblue", "darkcyan"
+  , "darkgoldenrod", "darkgray", "darkgreen", "darkgrey", "darkkhaki", "darkmagenta"
+  , "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen"
+  , "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet"
+  , "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick"
+  , "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod"
+  , "gray", "green", "greenyellow", "grey", "honeydew", "hotpink", "indianred", "indigo"
+  , "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue"
+  , "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightgrey"
+  , "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray"
+  , "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta"
+  , "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen"
+  , "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue"
+  , "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab"
+  , "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred"
+  , "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple"
+  , "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell"
+  , "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen"
+  , "steelblue", "tan", "teal", "thistle", "tomato", "transparent", "turquoise", "violet", "wheat", "white"
+  , "whitesmoke", "yellow", "yellowgreen"]
+
+colorProperties :: Array String
+colorProperties = ["-ms-scrollbar-3dlight-color"
+  , "-ms-scrollbar-arrow-color"
+  , "-ms-scrollbar-base-color"
+  , "-ms-scrollbar-darkshadow-color"
+  , "-ms-scrollbar-face-color"
+  , "-ms-scrollbar-highlight-color"
+  , "-ms-scrollbar-shadow-color"
+  , "-ms-scrollbar-track-color"
+  , "-moz-border-bottom-colors"
+  , "-moz-border-left-colors"
+  , "-moz-border-right-colors"
+  , "-moz-border-top-colors"
+  , "-webkit-border-before"
+  , "-webkit-border-before-color"
+  , "-webkit-box-reflect"
+  , "-webkit-mask"
+  , "-webkit-mask-image"
+  , "-webkit-tap-highlight-color"
+  , "-webkit-text-fill-color"
+  , "-webkit-text-stroke"
+  , "-webkit-text-stroke-color"
+  , "backdrop-filter"
+  , "background"
+  , "background-color"
+  , "background-image"
+  , "border"
+  , "border-block"
+  , "border-block-color"
+  , "border-block-end"
+  , "border-block-end-color"
+  , "border-block-start"
+  , "border-block-start-color"
+  , "border-bottom"
+  , "border-bottom-color"
+  , "border-color"
+  , "border-image"
+  , "border-image-source"
+  , "border-inline"
+  , "border-inline-end"
+  , "border-inline-color"
+  , "border-inline-end-color"
+  , "border-inline-start"
+  , "border-inline-start-color"
+  , "border-left"
+  , "border-left-color"
+  , "border-right"
+  , "border-right-color"
+  , "border-top"
+  , "border-top-color"
+  , "box-shadow"
+  , "caret-color"
+  , "color"
+  , "column-rule"
+  , "column-rule-color"
+  , "content"
+  , "filter"
+  , "mask"
+  , "mask-border"
+  , "mask-border-source"
+  , "mask-image"
+  , "outline"
+  , "outline-color"
+  , "scrollbar-color"
+  , "shape-outside"
+  , "text-decoration"
+  , "text-decoration-color"
+  , "text-emphasis"
+  , "text-emphasis-color"
+  , "text-shadow"
+  , "-webkit-mask-box-image"
+  , "fill"
+  , "stroke"
+  ]
+
 main :: Effect Unit
 main = do
   test "getValues" $ do
     expectToEqual (getValues "position") positionValues
     expectToEqual (getValues "invalid") []
+  describe "getValues of color properties" $ do
+    sequence_ $ (\p -> test p $ expectToEqual (getValues p) colorValues) <$> colorProperties
